1d4eb4cdacd21f663e293d6e5cbba8bdce359bf6
Joe Julian <me@joejulian.name>
2017-10-13 08:59:02 -0700

    debug how variables exist in shell

---
+++ build/build.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+echo "chart_name='$chart_name'"
+
+exit 1
+
+CHART_VER=$(git describe --tags --abbrev=0 | sed 's/^v//')
+CHART_REL=$(git rev-list --count v$${CHART_VER}..HEAD)
+
+CHART_VER=${CHART_VER:-0.0.1} CHART_REL=${CHART_REL:-0} \
+    envsubst < Chart.yaml.in > ${chart_name}/Chart.yaml
