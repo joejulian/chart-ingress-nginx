deb8428ac521c82dd04377cb23daa4ce054c5e41
Joe Julian <me@joejulian.name>
2017-10-13 09:22:53 -0700

    paths

--- build/build.sh
+++ build/build.sh
@@ -4,4 +4,4 @@ CHART_VER=$(git describe --tags --abbrev=0 | sed 's/^v//')
 CHART_REL=$(git rev-list --count v$${CHART_VER}..HEAD)
 
 CHART_VER=${CHART_VER:-0.0.1} CHART_REL=${CHART_REL:-0} \
-    envsubst < Chart.yaml.in > ${chart_name}/Chart.yaml
+    envsubst < build/Chart.yaml.in > ${chart_name}/Chart.yaml
